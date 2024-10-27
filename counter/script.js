3836935440afc3668cddd067185f2a72d08085ac
yusuftotic <yusuftotic@outlook.com>
2024-10-27 22:43:07 +0300

    Add event listeners

---
+++ counter/script.js
@@ -0,0 +1,18 @@
+const counter = document.getElementById('counter')
+const btnIncrease = document.getElementById('increase')
+const btnDecrease = document.getElementById('decrease')
+const btnReset = document.getElementById('reset')
+
+
+
+btnIncrease.addEventListener('click', (e) => {
+  console.log('+')
+})
+
+btnDecrease.addEventListener('click', (e) => {
+  console.log('-')
+})
+
+btnReset.addEventListener('click', (e) => {
+  console.log('reset')
+})
