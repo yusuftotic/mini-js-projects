cd35e0a7205c3e47bad1c675b75b6509f4e8a037
yusuftotic <yusuftotic@outlook.com>
2024-10-27 23:10:02 +0300

    Update counter decrease functionality using Math.max()

--- counter/script.js
+++ counter/script.js
@@ -3,9 +3,11 @@ const btnIncrease = document.getElementById('increase')
 const btnDecrease = document.getElementById('decrease')
 const btnReset = document.getElementById('reset')
 
-
+// Increase
 btnIncrease.addEventListener('click', () => counter.innerText = +counter.innerText + 1)
 
-btnDecrease.addEventListener('click', () => counter.innerText = counter.innerText > 0 ? +counter.innerText - 1 : 0)
+// Decrease
+btnDecrease.addEventListener('click', () => counter.innerText = Math.max(0, +counter.innerText - 1))
 
+// Reset
 btnReset.addEventListener('click', () => counter.innerText = 0)
